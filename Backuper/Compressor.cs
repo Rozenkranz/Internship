7a1eea4663bf64debf7ae0fbe6f8990af88eb701
Rozenkranz <komarov.pnz@mail.ru>
2023-09-02 23:41:06 +0300

    Разделены классы

---
+++ Backuper/Compressor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backuper
+{
+    static class Compressor
+    {
+        static public int CompressGZ(Config cfg)
+        {
+            int errors = 0;
+
+            string CurrentDir = CreateTemp(cfg.TargetPath, false);
+            DirectoryInfo directorySelected = new DirectoryInfo(cfg.HomePath);
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                try
+                {
+                    string FileTargetPath = CurrentDir + "/" + fileToCompress.Name;
+
+                    using (FileStream originalFileStream = fileToCompress.OpenRead())
+                    {
+                        if ((File.GetAttributes(fileToCompress.FullName) &
+                           FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                        {
+                            using (FileStream compressedFileStream = File.Create(FileTargetPath + ".gz"))
+                            {
+
+                                using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                                   CompressionMode.Compress))
+                                {
+                                    originalFileStream.CopyTo(compressionStream);
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+                catch
+                {
+                    errors++;
+                    continue;
+                }
+            }
+            return errors;
+        }
+
+        public static int CompressZip(Config cfg)
+        {
+            string CurrentDir = CreateTemp(cfg.TargetPath, true);
+            try
+            {
+                ZipFile.CreateFromDirectory(cfg.HomePath, CurrentDir);
+            }
+            catch
+            {
+                return 1;
+            }
+            return 0;
+        }
+        static public string CreateTemp(string TempPath, bool archive)
+        {
+            string DirName = TempPath
+                + "/Temp_" + $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}";
+            if (!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+            if (archive)
+                DirName += "/Temp_" + $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.zip";
+            return DirName;
+        }
+    }
+
+}
+
---
+++ Backuper/Config.cs
@@ -0,0 +1,17 @@
+﻿namespace Backuper
+{
+    class Config
+    {
+        public string TargetPath { get; set; }
+        public string HomePath { get; set; }
+        public string CompressionType { get; set; }
+
+        public Config()
+        {
+            TargetPath = null;
+            HomePath = null;
+            CompressionType = "gzip";
+        }
+
+    }
+}
